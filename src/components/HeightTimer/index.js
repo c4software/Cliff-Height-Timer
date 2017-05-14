8bf056d85a4c1bfdb6c1bb5035e64c27cfca7d48
Valentin Brosseau <c4software@gmail.com>
2017-05-14 22:36:12 +0200

    Add height calculation

--- src/components/HeightTimer/index.js
+++ src/components/HeightTimer/index.js
@@ -4,17 +4,42 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 class HeightTimer extends Component{
   constructor(props){
-    super(props)
-    this.state = {running: false};
+    super(props);
+    this.state = {running: false, time: 0, m: "0 m", ft: "0 ft"};
   }
 
   start = () => {
-      this.setState({running: !this.state.running});
+      this.setState({
+        start: new Date().getTime(),
+        running: !this.state.running
+      });
   };
+
+  compute = () => {
+      let fallTime = new Date().getTime() - this.state.start;
+  		let height = 16*Math.pow((fallTime/1000), 2);
+      let numberDigits = 0;
+			if(height < 1){
+				numberDigits = 1;
+			}
+
+      let fallTimeFormated = (fallTime/1000).toFixed(2)+"s";
+      let feetFormated = height.toFixed(numberDigits)+" ft";
+      let meterFormated = (height/3.2808).toFixed(numberDigits)+" m";
+
+      this.setState({
+        time: fallTimeFormated,
+        m: meterFormated,
+        ft: feetFormated
+      })
+  }
 
   render(){
     return (
       <div>
+        <h1>{this.state.time}</h1>
+        <h3>{this.state.m}</h3>
+        <h3>{this.state.ft}</h3>
         <RaisedButton fullWidth={true} secondary={this.state.running} onTouchTap={this.start} label={this.state.running?"Stop":"Start"} />
       </div>
     )
