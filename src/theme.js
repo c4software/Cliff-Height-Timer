2b2e5a900d271c7307311020a2f6876f581da218
Valentin Brosseau <c4software@gmail.com>
2017-05-16 19:57:09 +0200

    Add blue color for the theme

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {blue500} from "material-ui/styles/colors"
+
+const theme = getMuiTheme({
+    palette: {
+        primary1Color: blue500
+    }
+});
+
+export default theme;
